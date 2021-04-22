6760ec852f9400559823854b804695214e727b00
Brandon <bsantizo722@west-mec.org>
2021-04-22 15:58:17 -0700

    complete nixon project

---
+++ vote.js
@@ -0,0 +1,27 @@
+function voteHere() {
+    alert("Welcome to The 1972 election");
+}
+function myFunction() {
+    alert("Bro, You sure?");
+    alert ('Positivly sure?');
+    alert ('Estas seguro?');
+    alert('Fine whatever 🙄');
+}
+
+
+
+
+function bigHead(x) {
+    x.style.height = "800px";
+    x.style.width = "800px";
+}
+
+function smollHead(x) {
+    x.style.height = "40px";
+    x.style.width = "40px";
+}
+
+window.addEventListener('contextmenu',
+function (e){
+    e.preventDefault();
+}, false);
